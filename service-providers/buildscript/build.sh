68e6c1c5237f4a5344d40ee4af716f1ba6009c6b
shaikriyaz <shaikriyaz@automatedpros.io>
2025-05-07 16:35:38 +0530

    added appspec and buildscripts

--- service-providers/buildscript/build.sh
+++ service-providers/buildscript/build.sh
@@ -2,24 +2,24 @@
 
 #composer update
 
-#cd /var/www/html/external_service_provider && composer update --ignore-platform-reqs
+#cd /var/www/html/felidae-network/service-providers && composer update --ignore-platform-reqs
 
 
 
 #swagger generation 
-cd /var/www/html/external_service_provider && php artisan l5-swagger:generate 
+cd /var/www/html/felidae-network/service-providers && php artisan l5-swagger:generate 
 
 #Optimizing cache update 
 
-cd /var/www/html/external_service_provider && php artisan optimize:clear 
+cd /var/www/html/felidae-network/service-providers && php artisan optimize:clear 
 
 
 #Migration 
 
-cd /var/www/html/external_service_provider && php artisan migrate 
+cd /var/www/html/felidae-network/service-providers && php artisan migrate 
 
 
 #Optimizing cache update 
 
-cd /var/www/html/external_service_provider && php artisan cache:clear
+cd /var/www/html/felidae-network/service-providers && php artisan cache:clear
 
