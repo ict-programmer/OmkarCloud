4692305fb4c06dcd702ee4f5ff1c3fe5043a55d1
macbookpro <m.a.birakdar@gmail.com>
2025-06-23 18:55:52 +0300

    add auth for publish.io

--- service-providers/config/image.php
+++ service-providers/config/image.php
@@ -4,4 +4,6 @@ return [
     'base_url' => env('IMAGE_BASE_URL', 'https://publiish.io'),
     'max_file_size' => 500 * 1024, // 500MB
     'upload_time_out' => 0,
+    'default_email' => env("PUBLIISH_IO_EMAIL"),
+    'default_password' => env("PUBLIISH_IO_PASSWORD"),
 ];
