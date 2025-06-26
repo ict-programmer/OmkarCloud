b8b53a8ef1426e0495e279072c73c82352b5c12d
Kyaw Thiha <kyaw.thiha@automatedpros.io>
2025-06-26 20:02:35 +0700

    fix:perplexity seeder per latest doc

---
+++ service-providers/app/Models/ServiceProviderModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Eloquent\Model;
+
+class ServiceProviderModel extends Model
+{
+    /**
+     * The connection name for the model.
+     *
+     * @var string
+     */
+    protected $connection = 'mongodb';
+
+    /**
+     * The collection associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'service_provider_model';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'status',
+        'service_provider_id',
+        'service_type_id',
+    ];
+}
