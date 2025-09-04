b9c37043ce8861f526fe9f42fa764924cf7c1a97
ict-programmer <sm.ict.engineer@gmail.com>
2025-09-04 23:58:28 +0200

    feat(shotstack): add Shotstack API key configuration and update asset creation request validation

--- service-providers/app/Services/ShotstackService.php
+++ service-providers/app/Services/ShotstackService.php
@@ -5,7 +5,10 @@ namespace App\Services;
 use App\Data\Request\Shotstack\CheckRenderStatusData;
 use App\Data\Request\Shotstack\CreateAssetData;
 use App\Data\Request\Shotstack\GetVideoMetadataData;
+use App\Enums\common\ServiceProviderEnum;
+use App\Exceptions\ApiException;
 use App\Http\Exceptions\Forbidden;
+use App\Http\Exceptions\NotFound;
 use App\Http\Resources\Shotstack\CheckRenderStatusResource;
 use App\Http\Resources\Shotstack\CreateAssetResource;
 use App\Http\Resources\Shotstack\GetVideoMetadataResource;
@@ -14,6 +17,7 @@ use Exception;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ShotstackService
 {
@@ -33,9 +37,8 @@ class ShotstackService
       throw new NotFound('Shotstack API service provider not found.');
     }
 
-
+    throw_if(empty(config('services.shotstack.api_key')), new NotFound('Shotstack API key not found.'));
     $this->apiKey = config('services.shotstack.api_key');
-    throw_if(empty($this->apiKey), new NotFound('Shotstack API key not configured.'));
 
     $this->baseUrl = $provider->parameters['base_url'];
     $this->version = $provider->parameters['version'];
@@ -50,20 +53,20 @@ class ShotstackService
       $timeline = $this->buildTimeline($data->clips);
       $response = $this->client
         ->post("edit/{$this->version}/render", [
-          'json' => [
-            'timeline' => $timeline,
-            'output' => $data->output,
-          ],
+          'timeline' => $timeline,
+          'output' => $data->output,
         ]);
 
+      $parsedResponse = $response->json();
+
       if ($response->failed()) {
-        throw new \Exception('Failed to create asset');
+        throw new ApiException('Failed to create asset', $response->getStatusCode(), $parsedResponse);
       }
 
-      return CreateAssetResource::make($response->getData(true));
+      return CreateAssetResource::make($parsedResponse);
     } catch (ConnectionException | Exception $e) {
       Log::error('Shotstack request error: ' . $e->getMessage());
-      throw new Forbidden('Shotstack request failed');
+      throw $this->handleException($e);
     }
   }
 
@@ -71,29 +74,35 @@ class ShotstackService
   {
     try {
       $response = $this->client->get("edit/{$this->version}/render/{$data->id}");
+
+      $parsedResponse = $response->json();
+
       if ($response->failed()) {
-        throw new \Exception('Failed to check render status');
+        throw new ApiException('Failed to check render status', $response->getStatusCode(), $parsedResponse);
       }
 
-      return CheckRenderStatusResource::make($response->getData(true));
+      return CheckRenderStatusResource::make($response->json());
     } catch (ConnectionException | Exception $e) {
       Log::error('Shotstack request error: ' . $e->getMessage());
-      throw new Forbidden('Shotstack request failed');
+      throw $this->handleException($e);
     }
   }
 
   public function getVideoMetadata(GetVideoMetadataData $data): GetVideoMetadataResource
   {
     try {
-      $response = $this->client->get("serve/{$this->version}/assets/{$data->id}");
+      $response = $this->client->get("serve/{$this->version}/assets/render/{$data->id}");
+
+      $parsedResponse = $response->json();
+
       if ($response->failed()) {
-        throw new \Exception('Failed to get video metadata');
+        throw new ApiException('Failed to get video metadata', $response->getStatusCode(), $parsedResponse);
       }
 
-      return GetVideoMetadataResource::make($response->getData(true));
+      return GetVideoMetadataResource::make($response->json());
     } catch (ConnectionException | Exception $e) {
       Log::error('Shotstack request error: ' . $e->getMessage());
-      throw new Forbidden('Shotstack request failed');
+      throw $this->handleException($e);
     }
   }
 
@@ -103,12 +112,40 @@ class ShotstackService
 
     foreach ($clips as $clip) {
       $tracks[] = [
-        "clips" => $clip,
+        "clips" => [$clip],
       ];
     }
 
-    return ["timeline" => [
+    return  [
       "tracks" => $tracks,
-    ]];
+    ];
+  }
+
+  private function handleException(Exception $e): Exception
+  {
+    if ($e instanceof ApiException) {
+      $response = $e->details;
+      if ($e->getStatusCode() == 400 && $response && array_key_exists('response', $response) && array_key_exists('errors', $response['response'])) {
+        $message = $response['response']['message'];
+        $errors = [];
+        foreach ($response['response']['errors'] as $error) {
+          $errors[] = $error['message'];
+        }
+        return new ApiException($message, $e->getStatusCode(), $errors);
+      }
+
+      if ($e->getStatusCode() == 404) {
+        return new NotFound('Shotstack asset not found');
+      }
+
+      if ($e->getStatusCode() == 401 && $response && array_key_exists('errors', $response)) {
+        $message = $response['errors'][0]['detail'];
+        return new ApiException($message, $e->getStatusCode());
+      }
+
+      return $e;
+    }
+
+    return new Forbidden('Shotstack request failed: ' . $e->getMessage());
   }
 }
